b69e26f29f2cf663cf7e42b6ed0f617ad16985a1
Chris Bailey <chris@ghostly.io>
2020-07-24 22:35:51 -0400

    add scoreboard and bind player name and score to labels

--- src/game/GameViewController.java
+++ src/game/GameViewController.java
@@ -1,6 +1,9 @@
 package game;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,11 +12,27 @@ public class GameViewController implements Initializable {
     Player playerOne;
     Player playerTwo;
 
+    @FXML private Label playerOneNameLabel;
+
+    @FXML private Label playerOneScoreLabel;
+
+    @FXML private Label playerTwoNameLabel;
+
+    @FXML private Label playerTwoScoreLabel;
+
     public void initData(String playerOneName, String playerTwoName) {
         playerOne = new Player(playerOneName, 0);
         playerTwo = new Player(playerTwoName, 0);
 
-        System.out.println(playerOne.toString());
+        playerOneNameLabel.setText(playerOne.getName());
+        playerOneScoreLabel
+                .textProperty()
+                .bind(new SimpleStringProperty(Integer.toString(playerOne.getScore())));
+
+        playerTwoNameLabel.setText(playerTwo.getName());
+        playerTwoScoreLabel
+                .textProperty()
+                .bind(new SimpleStringProperty(Integer.toString(playerTwo.getScore())));
     }
 
     @Override
